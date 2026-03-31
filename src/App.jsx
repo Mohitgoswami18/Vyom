45bd396c4c6ac50afb1ed3fe8e90c88aabfa93cb
Mohitgoswami18 <mohitgoswami18326@gmail.com>
2026-03-31 22:32:12 +0530

    created  Login and signup component

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,21 @@ import TechStack from "./components/TechStack.jsx"
 import Demo from "./components/SeeItWork.jsx"
 import CTA from "./components/CTA.jsx"
 import Footer from "./components/Footer.jsx";
+import Signup from "./components/Signup.jsx";
+import Login from "./components/Login.jsx";
 
 const App = () => {
   return (
     <div className="bg-[#0A0A0A]">
-      <Navbar />
+      {/* <Navbar />
       <Hero></Hero>
       <Features></Features>
       <HowItWorks></HowItWorks>
       <TechStack></TechStack>
       <Demo></Demo>
       <CTA></CTA>
-      <Footer></Footer>
+      <Footer></Footer> */}
+      <Login></Login>
     </div>
   );
 }
